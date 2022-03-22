d7be59500cd2c72f2db94bbbf0d4641da346a31e
Mayur Nagdev <mayurnagdev123@users.noreply.github.com>
2022-03-22 09:25:05 +0530

    moved files to the root folder

--- sprint-retrospectives-client/src/components/Backrop/Backdrop.js
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-import './Backdrop.css'
-const Backdrop = props => {
-  return <div className='backdrop'>{props.children}</div>
-}
-export default Backdrop
--- sprint-retrospectives-client/src/socket_instance/socket_instance.js
+++
@@ -1,9 +0,0 @@
-import { io } from 'socket.io-client'
-const URL = '192.168.1.4:9000'
-const socket = io(URL)
-
-socket.onAny((event, ...args) => {
-  console.log(event, args)
-})
-
-export default socket
