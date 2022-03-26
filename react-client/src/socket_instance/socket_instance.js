fbc7069c669cad33c754ae482408250eac2b171a
Mayur Nagdev <mayurnagdev123@users.noreply.github.com>
2022-03-26 08:39:04 +0530

    removed console.log message

--- react-client/src/socket_instance/socket_instance.js
+++ react-client/src/socket_instance/socket_instance.js
@@ -2,8 +2,8 @@ import { io } from 'socket.io-client'
 const URL = 'https://mn-agile-retrospectives-server.herokuapp.com/'
 const socket = io(URL)
 
-socket.onAny((event, ...args) => {
-  console.log(event, args)
-})
+//socket.onAny((event, ...args) => {
+  //console.log(event, args)
+//})
 
 export default socket
